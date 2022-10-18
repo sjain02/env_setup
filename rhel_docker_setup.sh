bdc54463e13eb6a462ea84d2e94580ed01075c71
Saurabh Jain <sj652744@broadcom.net>
2022-10-18 15:30:47 +0530

    fixing typo in the command

--- rhel_docker_setup.sh
+++ rhel_docker_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum install -y yum_utils && \
+sudo yum install -y yum-utils && \
 sudo yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo && \
 sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin && \
 sudo systemctl start docker && \
